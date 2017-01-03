de7c3c0228a0671f09238e5c3f63bac9977e8b23
laboratory-vr <laboratory.vr@gmail.com>
2017-01-03 15:04:29 +0800

    Add Async cloud extraction(.ply) function.

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -3,37 +3,92 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/viz/vizcore.hpp>
 #include <kfusion/kinfu.hpp>
-#include <io/capture.hpp>
+#include <io/capture2.hpp>
+
+//#include <kfusion/src/internal.hpp>
+
+//#include <kfusion/types.hpp>
+
+//#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+#include <boost/thread/thread.hpp>
+
+#include "obj_writer.h"
 
 using namespace kfusion;
+namespace po        = boost::program_options;
+namespace po_style  = boost::program_options::command_line_style;
 
 struct KinFuApp
 {
     static void KeyboardCallback(const cv::viz::KeyboardEvent& event, void* pthis)
     {
-        KinFuApp& kinfu = *static_cast<KinFuApp*>(pthis);
+        KinFuApp& app = *static_cast<KinFuApp*>(pthis);
 
         if(event.action != cv::viz::KeyboardEvent::KEY_DOWN)
             return;
-
-        if(event.code == 't' || event.code == 'T')
-            kinfu.take_cloud(*kinfu.kinfu_);
-
-        if(event.code == 'i' || event.code == 'I')
-            kinfu.iteractive_mode_ = !kinfu.iteractive_mode_;
+        
+        app.KeyboardDispatcher(event.code);
+    }
+    
+    static bool setIntrinsicsExternal( const std::string& calibration_file, kfusion::Intr& output )
+    {
+        bool bSet = false;
+        try{
+            using namespace boost::property_tree;
+            ptree rootTree;
+            xml_parser::read_xml( calibration_file, rootTree );
+            
+            ptree caliTree = rootTree.get_child("SystemCalibration0.CameraIntrinsics-depth");
+            
+            std::string id  = caliTree.get<std::string>( "<xmlattr>.id" );
+            
+            float fx        = caliTree.get<float>( "<xmlattr>.f_x" );
+            float fy        = caliTree.get<float>( "<xmlattr>.f_y" );
+            float cx        = caliTree.get<float>( "<xmlattr>.c_x" );
+            float cy        = caliTree.get<float>( "<xmlattr>.c_y" );
+            
+            output.fx = fx;
+            output.fy = fy;
+            output.cx = cx;
+            output.cy = cy;
+            
+            //std::cout << "auto-intrinsic-values: fx: " << fx << " fy: " << fy << " cx: " << cx << " cy: " << cy << std::endl;
+            std::cout << "intrinsic loaded: : " << output << std::endl;
+            bSet = true;
+        }
+        catch(boost::property_tree::xml_parser::xml_parser_error& e){
+            std::cout << "Depth intrinsics config file is not exist or invalid." << std::endl;
+        }
+        return bSet;
     }
 
-    KinFuApp(OpenNISource& source) : exit_ (false),  iteractive_mode_(false), capture_ (source), pause_(false)
+
+    KinFuApp(OpenNI2Source& source) :  pause_(false), exit_ (false),
+        iteractive_mode_(false), capture_ (source), compute_normals_(true)
     {
         KinFuParams params = KinFuParams::default_params();
+        
+        params.intr         = kfusion::Intr(366.736, 366.736, 259.41, 204.374); //640f * 480f
+        //Vec3f volume_size   = Vec3f::all(1.2f);
+        float size_seed = .6f;
+        int   dim_seed  = (1 << 8);
+        Vec3f volume_size   = Vec3f( size_seed * 3.5, size_seed * 2, size_seed * 1.5 );
+        Vec3i volume_dims   = Vec3i( dim_seed  * 3.5, dim_seed  * 2, dim_seed  * 1.5 );
+        params.volume_size  = volume_size;
+        params.volume_pose  = Affine3f().translate(Vec3f(-volume_size[0]/2, -volume_size[1]/2, 0.7f));
+        params.volume_dims  = volume_dims;
+        
         kinfu_ = KinFu::Ptr( new KinFu(params) );
 
         capture_.setRegistration(true);
 
         cv::viz::WCube cube(cv::Vec3d::all(0), cv::Vec3d(params.volume_size), true, cv::viz::Color::apricot());
-        viz.showWidget("cube", cube, params.volume_pose);
-        viz.showWidget("coor", cv::viz::WCoordinateSystem(0.1));
-        viz.registerKeyboardCallback(KeyboardCallback, this);
+        viz_.showWidget("cube", cube, params.volume_pose);
+        viz_.showWidget("coor", cv::viz::WCoordinateSystem(0.1));
+        viz_.registerKeyboardCallback(KeyboardCallback, this);
     }
 
     void show_depth(const cv::Mat& depth)
@@ -43,12 +98,18 @@ struct KinFuApp
         depth.convertTo(display, CV_8U, 255.0/4000);
         cv::imshow("Depth", display);
     }
+    
+    void show_color(const cv::Mat& color)
+    {
+        //TODO: BGR to RGB
+        cv::imshow("Color", color);
+    }
 
     void show_raycasted(KinFu& kinfu)
     {
         const int mode = 3;
         if (iteractive_mode_)
-            kinfu.renderImage(view_device_, viz.getViewerPose(), mode);
+            kinfu.renderImage(view_device_, viz_.getViewerPose(), mode);
         else
             kinfu.renderImage(view_device_, mode);
 
@@ -56,14 +117,90 @@ struct KinFuApp
         view_device_.download(view_host_.ptr<void>(), view_host_.step);
         cv::imshow("Scene", view_host_);
     }
+    
+    void take_cam_pose()
+    {
+        //std::vector<cv::Affine3f> until;
+        //const std::vector<cv::Affine3f>& from = seqCameraEstimate_;
+        //std::vector<cv::Affine3f>::const_iterator it = from.cbegin();
+        //for ( ; it != from.cend(); ++it )
+        //    until.push_back(*it);
+        
+        cv::Mat cloud_cam_pose  = cv::Mat( 1, (int)seqCameraEstimate_.size(), CV_32FC4 );
+        Point*  cloud_cam_point = cloud_cam_pose.ptr<Point>();
+        
+        int idx = 0;
+        std::vector<cv::Affine3f>::const_iterator it = seqCameraEstimate_.cbegin();
+        for ( ; it != seqCameraEstimate_.cend(); ++it, ++idx )
+        {
+            Point* pi = &cloud_cam_point[idx];
+            cv::Vec3f t = it->translation();
+            memcpy( pi, &t, sizeof(t));
+            //it->rotate()();
+        }
+        std::string filename    = fetch_filename("Affine","ply");
+        savePLYFile ( filename, cloud_cam_point, 0, cloud_cam_pose.total(), 6);
+    }
 
     void take_cloud(KinFu& kinfu)
     {
-        cuda::DeviceArray<Point> cloud = kinfu.tsdf().fetchCloud(cloud_buffer);
-        cv::Mat cloud_host(1, (int)cloud.size(), CV_32FC4);
-        cloud.download(cloud_host.ptr<Point>());
-        viz.showWidget("cloud", cv::viz::WCloud(cloud_host));
-        //viz.showWidget("cloud", cv::viz::WPaintedCloud(cloud_host));
+        ScopeTime time("take_cloud");
+        boost::lock_guard<boost::mutex> lock(cloud_mem_lock_);
+        
+        cuda::DeviceArray<Point> cloud = kinfu.tsdf().fetchCloud(cloud_buffer_);
+        cloud_host_.create( 1, (int)cloud.size(), CV_32FC4 );
+        cloud.download(cloud_host_.ptr<Point>());
+        viz_.showWidget("cloud", cv::viz::WCloud(cloud_host_));
+        std::cout << "[INFO] Done Fetch Cloud. Point Size: " << cloud.size() << std::endl;
+      
+        if (compute_normals_)
+        {
+            kinfu.tsdf().fetchNormals (cloud, normal_buffer_);
+            normals_host_.create( 1, (int)cloud.size(), CV_32FC4 );
+            normal_buffer_.download(normals_host_.ptr<Normal>());
+        }
+        take_cam_pose();
+    }
+    
+    void save_cloud_async(std::string filename)
+    {
+        ScopeTime time("save_cloud_async");
+        boost::lock_guard<boost::mutex> lock(cloud_mem_lock_);
+        if (compute_normals_ && cloud_host_.total() == normals_host_.total() )
+            savePLYFile ( filename, cloud_host_.ptr<Point>(), normals_host_.ptr<Normal>(), cloud_host_.total(), 6);
+        else
+            savePLYFile ( filename, cloud_host_.ptr<Point>(), 0, cloud_host_.total(), 6);
+        std::cout << "[INFO] Complete saving Point cloud to " << filename << std::endl;
+    }
+    
+    void save_cloud()
+    {
+        if ( cloud_host_.empty() ){
+            std::cout << "[WARNING] Point cloud is empty." << std::endl;
+            return ;
+        }
+
+        std::string filename    = fetch_filename("cloud","ply");
+        std::cout << "[INFO] Started to save point cloud to " << filename << std::endl;
+
+        boost::thread thrd( boost::bind(&KinFuApp::save_cloud_async, this, filename) );
+        //thrd.join();
+    }
+    
+    void KeyboardDispatcher(const unsigned char& key)
+    {
+        switch(key)
+        {
+            case 't': case 'T' : take_cloud(*kinfu_); break;
+            case 'i': case 'I' : iteractive_mode_ = !iteractive_mode_; break;
+            case 'n': case 'N' : compute_normals_ = !compute_normals_,
+                std::cout << " compute_normals: " << compute_normals_ << std::endl; break;
+            case 's': case 'S' : save_cloud(); break;
+            case '0': case ')' : kinfu_->reset(); break;
+                
+            case 27: exit_ = true; break;       //ESC
+            case 32: pause_ = !pause_; break;   //SPACE
+        }
     }
 
     bool execute()
@@ -73,12 +210,21 @@ struct KinFuApp
         double time_ms = 0;
         bool has_image = false;
 
-        for (int i = 0; !exit_ && !viz.wasStopped(); ++i)
+        for (int i = 0; !exit_ && !viz_.wasStopped() ; ++i) //
         {
-            bool has_frame = capture_.grab(depth, image);
-            if (!has_frame)
+            int frame_type = capture_.grab(depth, image);
+            if (frame_type < 0)
                 return std::cout << "Can't grab" << std::endl, false;
-
+            
+            if ( true ) //Only accept depth image
+            {
+                if(!image.empty())
+                    show_color(image);
+                //continue; //std::cout << "skip color" << std::endl,
+            }
+            
+            if (depth_device_.empty())
+                std::cout << "Depth image cols: " << depth.cols << " rows: "<< depth.rows << std::endl;
             depth_device_.upload(depth.data, depth.step, depth.rows, depth.cols);
 
             {
@@ -88,39 +234,53 @@ struct KinFuApp
 
             if (has_image)
                 show_raycasted(kinfu);
+            else
+                seqCameraEstimate_.clear(), seqCameraEstimate_.reserve(30000);
 
             show_depth(depth);
             //cv::imshow("Image", image);
-
+            
+            cv::Affine3f current_cam_pos = kinfu.getCameraPose();
+            seqCameraEstimate_.push_back(current_cam_pos);
+            
             if (!iteractive_mode_)
-                viz.setViewerPose(kinfu.getCameraPose());
+                viz_.setViewerPose(current_cam_pos);
 
             int key = cv::waitKey(pause_ ? 0 : 3);
 
-            switch(key)
-            {
-            case 't': case 'T' : take_cloud(kinfu); break;
-            case 'i': case 'I' : iteractive_mode_ = !iteractive_mode_; break;
-            case 27: exit_ = true; break;
-            case 32: pause_ = !pause_; break;
-            }
+            this->KeyboardDispatcher( (const unsigned char) key);
 
             //exit_ = exit_ || i > 100;
-            viz.spinOnce(3, true);
+            viz_.spinOnce(3, true);
         }
         return true;
     }
 
     bool pause_ /*= false*/;
     bool exit_, iteractive_mode_;
-    OpenNISource& capture_;
+    
+    OpenNI2Source& capture_;
     KinFu::Ptr kinfu_;
-    cv::viz::Viz3d viz;
+    cv::viz::Viz3d viz_;
 
     cv::Mat view_host_;
     cuda::Image view_device_;
     cuda::Depth depth_device_;
-    cuda::DeviceArray<Point> cloud_buffer;
+    
+    // downloader
+    
+    cuda::DeviceArray<Point> cloud_buffer_;
+    cv::Mat cloud_host_;
+    
+    bool compute_normals_;
+    cuda::DeviceArray<Normal> normal_buffer_;
+    cv::Mat normals_host_;
+
+    boost::mutex cloud_mem_lock_;
+    
+    // Affine3f recorder
+    
+    std::vector<cv::Affine3f> seqCameraEstimate_;
 };
 
 
@@ -130,13 +290,15 @@ int main (int argc, char* argv[])
 {
     int device = 0;
     cuda::setDevice (device);
+    //cuda::printCudaDeviceInfo (device);
     cuda::printShortCudaDeviceInfo (device);
 
     if(cuda::checkIfPreFermiGPU(device))
         return std::cout << std::endl << "Kinfu is not supported for pre-Fermi GPU architectures, and not built for them by default. Exiting..." << std::endl, 1;
-
-    OpenNISource capture;
-    capture.open (0);
+    
+    OpenNI2Source capture;
+    capture.open ("");
+    
     //capture.open("d:/onis/20111013-224932.oni");
     //capture.open("d:/onis/reg20111229-180846.oni");
     //capture.open("d:/onis/white1.oni");
---
+++ apps/obj_writer.cpp
@@ -0,0 +1,222 @@
+#include <kfusion/types.hpp>
+
+#include <boost/filesystem.hpp> //-->background threading 
+
+//#include <kfusion/kinfu.hpp>
+//#include <opencv2/core/core.hpp>
+
+#include "obj_writer.h"
+
+#include <iostream>
+#include <string>
+
+#define REPORT_ERROR(msg) kfusion::cuda::error ((msg), __FILE__, __LINE__)
+
+using namespace kfusion;
+
+std::string gen_next_filename(const std::string& filebase, const std::string& ext, const int& index)
+{
+    std::ostringstream os;
+    os << filebase << "_" << index << "." << ext;
+    
+    return os.str();
+}
+
+std::string fetch_filename(const std::string& filebase, const std::string& ext)
+{
+    int index = 0;
+    std::string filename = gen_next_filename(filebase, ext, index);
+    
+    while( is_file_exists(filename) )
+    {
+        filename = gen_next_filename(filebase, ext, ++index);
+    }
+    return filename;
+    
+}
+
+int
+saveOBJFile (const std::string &file_name,
+             const Point* cloud,
+             const Normal* normal,
+             unsigned cloud_size,
+             unsigned precision)
+{
+    if (0 >= cloud_size)
+    {
+        REPORT_ERROR ("[demo::saveOBJFile] Input point cloud has no data!\n");
+        return (-1);
+    }
+    // Open file
+    std::ofstream fs;
+    fs.precision (precision);
+    fs.open (file_name.c_str ());
+    
+    /* Write 3D information */
+    // number of points
+    int nr_points  = cloud_size;
+    // point size
+    //unsigned point_size = sizeof(Point);
+    // number of faces for header
+    unsigned nr_faces = 0;
+    // Do we have vertices normals?
+    int normal_index = normal? 0 : -1;
+    
+    // Write the header information
+    fs << "####" << std::endl;
+    fs << "# OBJ dataFile simple version. File name: " << file_name << std::endl;
+    fs << "# Vertices: " << nr_points << std::endl;
+    if (normal_index != -1)
+        fs << "# Vertices normals : " << nr_points << std::endl;
+    fs << "# Faces: " << nr_faces << std::endl;
+    fs << "####" << std::endl;
+    
+    // Write vertex coordinates
+    fs << "# List of Vertices, with (x,y,z) coordinates, w is optional." << std::endl;
+    for (int i = 0; i < nr_points; ++i)
+    {
+        // write vertices beginning with v
+        //const Point* pi = cloud + i * point_size;
+        const Point* pi = &cloud[i];
+        if (std::isfinite(pi->data[0]))
+        {
+            fs << "v " << pi->data[0] << " " << pi->data[1] << " " << pi->data[2] << std::endl;
+        }
+    }
+    
+    fs << "# "<< nr_points <<" vertices" << std::endl;
+    
+    if(normal_index != -1)
+    {
+        fs << "# Normals in (x,y,z) form; normals might not be unit." <<  std::endl;
+        for (int i = 0; i < nr_points; ++i)
+        {
+            // Write vertex normals
+            // write vertices beginning with v
+            const Normal* ni = &normal[i];
+            if (std::isfinite(ni->data[0]))
+            {
+                fs << "vn " << ni->data[0] << " " << ni->data[1] << " " << ni->data[2] << std::endl;
+            }
+        }
+        fs << "# "<< nr_points <<" vertices normals" << std::endl;
+    }
+
+    //fs << "# Face Definitions" << std::endl;
+
+    // Write down faces
+    /*
+    if(normal_index == -1)
+    {
+     for(unsigned i = 0; i < nr_faces; i++)
+     {
+        fs << "f ";
+      size_t j = 0;
+      for (; j < mesh.polygons[i].vertices.size () - 1; ++j)
+        fs << mesh.polygons[i].vertices[j] + 1 << " ";
+      fs << mesh.polygons[i].vertices[j] + 1 << std::endl;
+    }
+  }
+  else
+  {
+    for(unsigned i = 0; i < nr_faces; i++)
+    {
+      fs << "f ";
+      size_t j = 0;
+      for (; j < mesh.polygons[i].vertices.size () - 1; ++j)
+        fs << mesh.polygons[i].vertices[j] + 1 << "//" << mesh.polygons[i].vertices[j] + 1 << " ";
+      fs << mesh.polygons[i].vertices[j] + 1 << "//" << mesh.polygons[i].vertices[j] + 1 << std::endl;
+    }
+  }
+  */
+  fs << "# End of File" << std::endl;
+
+  // Close obj file
+  fs.close ();
+  return 0;
+}
+
+int
+savePLYFile (const std::string &file_name,
+             const Point* cloud,
+             const Normal* normal,
+             unsigned cloud_size,
+             unsigned precision,
+             const std::string& format)
+{
+    if (0 >= cloud_size)
+    {
+        REPORT_ERROR ("[demo::saveOBJFile] Input point cloud has no data!\n");
+        return (-1);
+    }
+    
+    /*
+     ply
+     format ascii 1.0
+     comment VCGLIB generated
+     element vertex 1004187
+     property float x
+     property float y
+     property float z
+     property float nx
+     property float ny
+     property float nz
+     element face 0
+     property list uchar int vertex_indices
+     end_header
+     */
+    
+    
+    // Open file
+    std::ofstream fs;
+    fs.precision (precision);
+    fs.open (file_name.c_str ());
+    
+    /* Write 3D information */
+    // number of points
+    int nr_points  = cloud_size;
+    // point size
+    // number of faces for header
+    unsigned nr_faces = 0;
+    // Do we have vertices normals?
+    int normal_index = normal? 1 : 0;
+    
+    // Write the header information
+    fs << "ply" << std::endl;
+    fs << "format " << format << " 1.0" << std::endl;
+    fs << "comment AEMASS generated file: " << file_name << std::endl;
+    fs << "element vertex " << nr_points << std::endl;
+    fs << "property float x" << std::endl;
+    fs << "property float y" << std::endl;
+    fs << "property float z" << std::endl;
+    
+    if (normal_index)
+    {
+        fs << "property float nx" << std::endl;
+        fs << "property float ny" << std::endl;
+        fs << "property float nz" << std::endl;
+    }
+    fs << "element face " << nr_faces << std::endl;
+    fs << "property list uchar int vertex_indices" << std::endl;
+    fs << "end_header" << std::endl;
+    
+    // Write vertex coordinates
+    for (int i = 0; i < nr_points; ++i)
+    {
+        const Point*  pi = &cloud[i];
+        const Normal* ni = &normal[i];
+        if (std::isfinite(pi->data[0]))
+        {
+            fs << pi->data[0] << " " << pi->data[1] << " " << pi->data[2];
+            if(normal_index)
+            {
+                fs << " " << ni->data[0] << " " << ni->data[1] << " " << ni->data[2];
+            }
+            fs << std::endl;
+        }
+    }
+    fs << std::endl;
+    // Close obj file
+    fs.close ();
+    return 0;
+}
